693c13667c29056e541afe543217e33d2c148251
AlekEl <leszczynski.al@gmail.com>
2018-10-29 10:51:00 +0100

    Connected all them buttons to controller

--- src/tamagotchiGame/controller/Controller.java
+++ src/tamagotchiGame/controller/Controller.java
@@ -6,7 +6,15 @@ import javafx.fxml.FXML;
 public class Controller {
 
     @FXML protected void handleFoodButtonPressed(ActionEvent event) {
-        System.out.println("Pressed!");
+        System.out.println("Food Pressed!");
+    }
+
+    @FXML protected void handleSleepButtonPressed(ActionEvent event) {
+        System.out.println("Sleep Pressed!");
+    }
+
+    @FXML protected void handlePlayButtonPressed(ActionEvent event) {
+        System.out.println("Play Pressed!");
     }
 
 }
